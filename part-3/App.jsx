461847805f9e1c239bb838ca10b54a3075051305
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-27 12:40:31 -0700

    part3

---
+++ part-3/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+    return (
+        <div>
+            <Person
+                name="FirstLast"
+                hobbies={["tennis", "biking"]}
+                age={18}
+            />
+            <Person
+                name="Fire"
+                hobbies={["tennis", "biking"]}
+                age={20}
+            />
+             <Person
+                name="F"
+                hobbies={["tennis", "biking"]}
+                age={7}
+            />
+        </div>
+    );
+}
