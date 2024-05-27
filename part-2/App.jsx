72a1a06c7c0891507faba95e68fbd8d2485a3a0b
Zach Bunting <zvrbunting@gmail.com>
2024-05-27 12:21:20 -0700

    Part2.

---
+++ part-2/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+    return (
+        <div>
+            <Tweet
+                username="testUser"
+                name="FirstLast"
+                date="2024-05-27"
+                message="test msg1"
+            />
+            <Tweet
+                username="testUser2"
+                name="First2Last2"
+                date="2024-05-27"
+                message="test msg2"
+            />
+            <Tweet
+                username="testUser3"
+                name="First3Last3"
+                date="2024-05-27"
+                message="test msg3"
+            />
+        </div>
+    );
+}
